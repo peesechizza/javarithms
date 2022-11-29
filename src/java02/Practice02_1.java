2d68f69569190e6b32d21697c8ba0e0909d2a4d5
a1dotls <meeae9408@gmail.com>
2022-11-29 17:52:25 +0900

    연습문제 2-1

---
+++ src/java02/Practice02_1.java
@@ -0,0 +1,21 @@
+package java02;
+
+import java.util.Scanner;
+
+public class Practice02_1 {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        int sum = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("5개의 숫자를 입력.");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+            sum += arr[i];
+        }
+
+        System.out.print("모든 요소의 합: " + sum);
+    }
+}
